21188d4c2a51985f10aec937ba5ed0c6e44b2d39
rval13v <livarfpv@gmail.com>
2025-08-06 13:23:53 +0300

    new selenium+safari.py

--- selenium_safari.py
+++ selenium_safari.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import time
-
 from selenium.webdriver.common.by import By
 
 # Создаем драйвер Safari
@@ -15,19 +14,19 @@ driver.get(base_url)
 # Устанавливаем размер окна браузера
 driver.set_window_size(1920, 1080)
 
-# Здесь происходит поиск элемента на веб-странице по его атрибуту id, который равен "user-name".
-user_name = driver.find_element(By.ID, "user-name")
-
-# В это поле (user_name) вводится текст "standard_user". Это логин для тестового аккаунта.
+# Находим поле ввода имени пользователя по XPath
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys("standard_user")
 
-#Аналогично, находится поле ввода пароля по id="password", и элемент сохраняется в переменную user_password.
-user_password = driver.find_element(By.ID, "password")
+# Находим поле ввода пароля по ID
+user_password = driver.find_element(By.XPATH, "//input[@id='password']")
 user_password.send_keys("secret_sauce")
 
-# Находим оп id кнопку Логин и кликаем ее.
-login_button = driver.find_element(By.ID, "login-button")
+# Находим кнопку логина по ID и нажимаем её
+login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
 login_button.click()
 
-# time.sleep(5)
-# driver.close()# Закрываем окно браузера
+# Ждём немного, чтобы увидеть результат
+time.sleep(5)
+# Закрываем окно браузера
+driver.close()
