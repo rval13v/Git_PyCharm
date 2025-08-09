338d60dfb78918e57a8060f45befee75d441ae0e
rval13v <livarfpv@gmail.com>
2025-08-09 19:42:36 +0300

    selenium delete

---
+++ selenium_delete.py
@@ -0,0 +1,43 @@
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+#options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys("standa_user")
+print('Input Login')
+time.sleep(2)
+
+user_name.send_keys(Keys.COMMAND, 'a')
+user_name.send_keys(Keys.DELETE)
+
+user_password = driver.find_element(By.XPATH, "//input[@id='password']")
+user_password.send_keys("sec_sauce")
+print('Input password')
+time.sleep(2)
+
+user_password.send_keys(Keys.COMMAND, 'a')
+user_password.send_keys(Keys.DELETE)
+
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys("standard_user")
+
+user_password = driver.find_element(By.XPATH, "//input[@id='password']")
+user_password.send_keys("secret_sauce")
+
+user_password.send_keys(Keys.ENTER)
+
+
