29af26fbf31100e7a520660cc574bd2961d133f4
rval13v <livarfpv@gmail.com>
2025-08-12 20:15:13 +0300

    selenium checkbox

---
+++ selenium_checkbox.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+#options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+base_url = 'https://demoqa.com/checkbox'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+
+checkbox = driver.find_element(By.XPATH, "//span[@class='rct-checkbox']")
+checkbox.click()
+checkbox.is_selected()
+print('checkbox is selected')
