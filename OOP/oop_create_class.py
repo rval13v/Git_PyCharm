3ef947c30df9f154fe40de6c3b65c24c1cb4ca8c
rval13v <livarfpv@gmail.com>
2025-09-09 16:55:05 +0300

    selenium oop1 upd2

--- OOP/oop_create_class.py
+++ OOP/oop_create_class.py
@@ -4,19 +4,19 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-
 # Создаем общий класс
 class Test(): #создаем метод и помещаем в него выызов д-ра, url и открытие страницы
     def __init__(self): # коструктор класса чтобы создать и сохранить экземпляр браузера сразу при создании объекта класса. все методы могцт использоывать один браузер
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        self.base_url = 'https://www.saucedemo.com/'
 
     def test_select_product(self):
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-        base_url = 'https://www.saucedemo.com/'
-        self.driver.get(base_url)
+        self.driver.get(self.base_url)
         self.driver.maximize_window()
+        time.sleep(2)
 
     def login(self):
         user_name = self.driver.find_element(By.XPATH, "//input[@id='user-name']")
