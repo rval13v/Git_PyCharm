5bea02a95b03a7f77a3ea3919f7699f89a5fe6f6
rval13v <livarfpv@gmail.com>
2025-09-09 15:59:01 +0300

    selenium oop1 upd

--- OOP/oop_create_class.py
+++ OOP/oop_create_class.py
@@ -9,24 +9,28 @@ options.add_experimental_option("detach", True)
 
 # Создаем общий класс
 class Test(): #создаем метод и помещаем в него выызов д-ра, url и открытие страницы
+    def __init__(self): # коструктор класса чтобы создать и сохранить экземпляр браузера сразу при создании объекта класса. все методы могцт использоывать один браузер
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
     def test_select_product(self):
-
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
         base_url = 'https://www.saucedemo.com/'
-        driver.get(base_url)
-        driver.maximize_window()
-        time.sleep(2)
+        self.driver.get(base_url)
+        self.driver.maximize_window()
 
-        user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+    def login(self):
+        user_name = self.driver.find_element(By.XPATH, "//input[@id='user-name']")
         user_name.send_keys("standard_user")
-        user_password = driver.find_element(By.XPATH, "//input[@id='password']")
+        user_password = self.driver.find_element(By.XPATH, "//input[@id='password']")
         user_password.send_keys("secret_sauce")
-        login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
+        login_button = self.driver.find_element(By.XPATH, "//input[@id='login-button']")
         login_button.click()
-        time.sleep(2)
 
-        driver.quit()
+    def quit_driver(self):
+        self.driver.quit()
 
 # Создаем экземпляр класса
 start_test = Test()
 start_test.test_select_product()  # открываем страницу
+start_test.login()
+start_test.quit_driver()
