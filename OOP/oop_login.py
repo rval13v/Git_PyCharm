a664e5ac95913b8d8829ebfa00068a42ea7d739e
rval13v <livarfpv@gmail.com>
2025-09-14 19:46:43 +0300

    selenium oop login

---
+++ OOP/oop_login.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+USER_NAME = (By.XPATH, "//*[@id='user-name']")
+PASSWORD = (By.XPATH, "//*[@id='password']")
+LOGIN_BUTTON = (By.XPATH, "//*[@id='login-button']")
+
